f7c780c562bd030caad8476243a2a6f10d0e89aa
matthew rendely <matthew.rendely@gmail.com>
2023-03-15 19:48:56 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record){
+  let winYear = record.find(win);
+  return winYear ? winYear.year : undefined;
+
+} 
+
+function win(obj){
+  return obj.result === "W"
+}
